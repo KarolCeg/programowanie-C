0114cd6a2daab7cc437ac0ca979b1e491900aad8
KarolCeg <149320445+KarolCeg@users.noreply.github.com>
2023-11-28 00:05:56 +0100

    Update konwersja.c

--- konwersja.c
+++ konwersja.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
-int konwersja(char wiersz , char kolumna, int rozmiar)
+int konwersja(char kolumna , char wiersz, int rozmiar)
 {
 int pozycja = 0;
-printf("podaj pole na ktorym chcesz postawic pionek (wiersz , kolumna)\n");
-//scanf("%c %c", &wiersz , &kolumna); 
-wiersz = 'C';
-kolumna = 3;
-wiersz = wiersz - 64;  //tablica od zera do 8
- pozycja = wiersz*(rozmiar-1) +kolumna;
+printf("podaj pole na ktorym chcesz postawic pionek (kolumna , wiersz)(np A1)\n"); 
+kolumna = getchar();
+getchar();
+wiersz = getchar();
+wiersz -='0';
+kolumna -= 'A';  
+printf("%d , %d\n", kolumna + 1 , wiersz);
+wiersz--;
+pozycja = wiersz*rozmiar +kolumna;
  return pozycja;
 }
 void main()
 {
-    char wiersz = 0 , kolumna = 0;   
-    printf("%d", konwersja(wiersz,kolumna,3));
+    char wiersz  , kolumna ;   
+    printf("%d", konwersja(kolumna,wiersz,13));
 }
-//trzeba konwersję A,B C ... na wartosci 1,2,3,...
-//65-(wartosc zera w ascii)
