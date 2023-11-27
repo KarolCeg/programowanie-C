6c5cd329e2a2c5f089c1f22f61c0e35b124c3e6f
KarolCeg <149320445+KarolCeg@users.noreply.github.com>
2023-11-28 00:24:24 +0100

    Update konwersja.c

--- konwersja.c
+++ konwersja.c
@@ -3,19 +3,12 @@ int konwersja(char kolumna , char wiersz, int rozmiar)
 {
 int pozycja = 0;
 printf("podaj pole na ktorym chcesz postawic pionek (kolumna , wiersz)(np A1)\n"); 
-kolumna = getchar();
-getchar();
-wiersz = getchar();
-wiersz -='0';
-kolumna -= 'A';  
-printf("%d , %d\n", kolumna + 1 , wiersz);
-wiersz--;
-pozycja = wiersz*rozmiar +kolumna;
- return pozycja;
+scanf("%c%d", &kolumna , &wiersz); 
+pozycja = (wiersz - 1)*rozmiar + kolumna - 'A';
+return pozycja;
 }
 void main()
 {
     char wiersz  , kolumna ;   
     printf("%d", konwersja(kolumna,wiersz,13));
 }
-dwucyfrowe cyfry w ascii
